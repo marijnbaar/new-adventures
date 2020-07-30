b577f1555466a755ed5e9d705f8def9f87dbe009
Marijn Baar <marijn.baar@gmail.com>
2020-07-30 19:32:47 +0200

    fixed the functionality of the input field

--- client/src/App.js
+++ client/src/App.js
@@ -4,40 +4,57 @@ import Hotspot from './cities'
 
 
 class App extends Component {
-
-  state = {
-    hotspots: null,
-    keys: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotspots: null,
+      keys: null
+    };
   }
 
-  getCity = () => {
-    fetch(`http://localhost:3002/api/cities/${this.state.hotspots}`)
-      .then(res => res.json())
-      .then(data => this.setState({
-        keys: data.hotspots.walk,
-
-        // hotspots: data.hotspots
-      }))
-      .catch(err => err);
-  }
 
   callCities = () => {
-    console.log("try")
     let city = document.getElementById("city").value;
-    // fetch(`http://localhost:3002/api/cities/${city}`)
-    //   .then(res => res.json())
-    //   .then(data => {
-        this.setState({
-        // keys: data.hotspots.keys(),
-        hotspots: city
-      })
-      // .then(console.log('try2', this.state.hotspots))
-    // })
-      // .catch(err => err);
-    this.getCity();
+    console.log('see if this works', city)
+    fetch(`http://localhost:3002/api/cities/${city.toLowerCase()}`)
+      .then(res => res.json())
+      .then(data => this.setState({
+        keys: Object.keys(data.hotspots),
+        hotspots: data.hotspots
+      }))
+      .catch(err => console.log(err));
   }
 
+  componentDidMount() {
+    this.callCities();
+  }
 
+  // getCity = () => {
+  //   fetch(`http://localhost:3002/api/cities/${this.state.hotspots}`)
+  //     .then(res => res.json())
+  //     .then(data => this.setState({
+  //       keys: data.hotspots.walk,
+
+  //       // hotspots: data.hotspots
+  //     }))
+  //     .catch(err => err);
+  // }
+
+  // callCities = () => {
+  //   console.log("try")
+  //   let city = document.getElementById("city").value;
+  //   // fetch(`http://localhost:3002/api/cities/${city}`)
+  //   //   .then(res => res.json())
+  //   //   .then(data => {
+  //       this.setState({
+  //       // keys: data.hotspots.keys(),
+  //       hotspots: city
+  //     })
+  //     // .then(console.log('try2', this.state.hotspots))
+  //   // })
+  //     // .catch(err => err);
+   
+  // }
 
 
 
@@ -47,14 +64,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
-      <p>{this.state.hotspots}</p>
-      <p>{this.state.keys}</p>
       <input type="text" id="city"></input><button onClick={this.callCities.bind(this)}>click me</button>
         </header>
         <main>
           <div>
+          {/* <p>{this.state.hotspots ? this.state.hotspots.walk : 'hi'}</p> */}
+
             {
-              this.state.keys ? this.state.keys.map(key => (<Hotspot action={key} suggestion={this.state.hotspots.key}/>)) :
+              this.state.keys ? this.state.keys.map(key => (<Hotspot action={key} suggestion={this.state.hotspots[key]}/>)) :
               null
             }
           </div>
